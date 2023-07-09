40c09a2278555c75df72a824249a20f246b67274
Alejandro Timpano <alejandro.timpano@proyecciones.local>
2023-07-09 18:43:02 -0300

    Primera entrega V2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header } from "./components/Header/Header"
 import './App.css'
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import reactIcon from './assets/react.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <ItemListContainer saludo={"Bienvenidos"}/>
+
+        <img src="/public/vite.svg" alt="una imagen" />
+        <img src={reactIcon} alt="una imagen" />
+
+        <Button variant="dark">Click</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.scss'
+export const Header = () => {
+
+return (
+<header className="header">
+    <div className="header__container">
+        <h1 className="header__logo"><i>ELIJO CREER</i></h1>
+
+        <nav className="navbar">
+            <a className="navbar__link" href="#">Enlace 1</a>
+            <a className="navbar__link" href="#">Enlace 2</a>
+            <a className="navbar__link" href="#">Enlace 3</a>
+        </nav>
+    </div>
+</header>
+)
+}
