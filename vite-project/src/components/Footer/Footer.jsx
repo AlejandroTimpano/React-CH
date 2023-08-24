97a8f60482459f320875ff21ce8c691e964c0264
Alejandro Timpano <alejandro.timpano@proyecciones.local>
2023-08-23 22:54:11 -0300

    version final 3

--- vite-project/src/components/Footer/Footer.jsx
+++ vite-project/src/components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ export const Footer = () => {
                 <h1 className="footer__logo">"Todos cantan, todos se abrazan, todos corren, todos lloramos. La selección ganó la final y puso otra vez al fútbol argentino como el mejor del mundo. Un día del que no nos olvidaremos por el resto de nuestras vidas".
     Copyright © 2022. Asociación del Fútbol Argentino. Todos los derechos reservados a Alejandro Timpano.</h1>
                 <CartWidget />
+                
             </div>
         </footer>
     )
